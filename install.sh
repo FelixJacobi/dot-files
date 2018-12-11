e11e11e124a6c455d8c0596036031019013ac1ac
Felix Jacobi <Felix@jacobi-hamburg.net>
2018-12-11 04:41:33 +0100

    Added basic install script

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh -e
+
+DEST="$HOME/"
+
+for f in $(find -type f -not \( -path './.git/*' -or -path './install.sh' -or -path './README.md' -or -path './LICENSE' \))
+do
+  rf="$(realpath -sm "$f")"
+  rd="$(realpath -sm "$DEST/$f")"
+  echo $f
+
+  if [ -L "$rd" ]
+  then
+    echo "Replacing existing symlink $rd with one to $rf."
+    ln -sf "$rf" "$rd"
+    continue
+  fi
+
+  if ! [ -f "$rd" ] 
+  then
+    echo "Creating new symlink $rd to $rf."
+    ln -sf "$rf" "$rd"
+    continue
+  fi
+
+  echo "$rd is a real file. Difference with $rf:"
+  diff -u "$DEST/$f" "$rf"
+  replace=n
+
+  echo -n "Do you want to replace it? (y/N) "
+  read replace
+
+  if [ "$replace" = "y" ] || [ "$replace" = "Y" ]
+  then
+    echo "Replacing file $rd with symlink to $rf."
+    ln -sf "$rf" "$rd"
+    continue    
+  fi
+done
