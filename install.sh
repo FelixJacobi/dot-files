98e7473750db6b57d662458f706825cefd325854
Felix Jacobi <felix.jacobi@iserv.eu>
2018-12-20 04:27:20 +0100

    install.sh: Added support for UID based home and environment specific drop-ins

--- install.sh
+++ install.sh
@@ -1,25 +1,46 @@
-#!/bin/sh -e
+#!/bin/bash -e
+set -e
 
 DEST="$HOME/"
+# config destination for programs that resolves home by UID (SSH -.-")
+DEST_UID="$(zsh -c "echo ~$(getent passwd "0" | cut -d: -f1)")"
 
-for f in $(find -type f -not \( -path './.git/*' -or -path './install.sh' -or -path './README.md' -or -path './LICENSE' \))
-do
-  rf="$(realpath -sm "$f")"
-  rd="$(realpath -sm "$DEST/$f")"
-  echo $f
+# paths which will be only copied once (NOT symlinked!)
+is_oneshot()
+{
+  [ "$1" = "./uid/.ssh/config.real" ] || [ "$1" = "./uid/.ssh/config" ]
+}
+
+# get a specific replacement for the given file
+get_drop_in()
+{
+  # IServ configuration
+  if [ -x /usr/sbin/iservchk ] && [ -f "./iserv/$1" ]
+  then
+    echo "$PWD/iserv/$1"
+    return
+  fi
+
+  echo "$2"
+}
+
+install_file()
+{
+  rf="$1"
+  rd="$2"
 
   if [ -L "$rd" ]
   then
     echo "Replacing existing symlink $rd with one to $rf."
     ln -sf "$rf" "$rd"
-    continue
+    return
   fi
 
-  if ! [ -f "$rd" ] 
+  if ! [ -e "$rd" ]
   then
     echo "Creating new symlink $rd to $rf."
     ln -sf "$rf" "$rd"
-    continue
+    return
   fi
 
   echo "$rd is a real file. Difference with $rf:"
@@ -33,6 +54,34 @@ do
   then
     echo "Replacing file $rd with symlink to $rf."
     ln -sf "$rf" "$rd"
-    continue    
+    return
   fi
+}
+
+for f in $(find -type f -not \( -path './uid/*' -or -path './.git/*' -or -path './install.sh' -or -path './README.md' -or -path './LICENSE' -or -path './iserv/*' \))
+do
+  rf="$(realpath -sm "$f")"
+  rd="$(realpath -sm "$DEST/$f")"
+  echo $f
+
+  install_file "$(get_drop_in "$f" "$rf")" "$rd"
+done
+
+for f in $(find -type f -path './uid/*')
+do
+  rf="$(realpath -sm "$f")"
+  rd="$(realpath -sm "$DEST_UID/$(echo $f | sed -E 's#^\./uid##g')")"
+  echo $f
+
+  if is_oneshot "$f"
+  then
+    if [ ! -e "$rd" ]
+    then
+        cp -av "$(get_drop_in "$f" "$rf")" "$rd"
+    fi
+
+    continue
+  fi
+
+  install_file "$(get_drop_in "$f" "$rf")" "$rd"
 done
